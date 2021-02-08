810f2d72e1ae24ebd6ed8733aa2a5c5f4e3be3d4
synergit <syner.wang@gmail.com>
2021-02-07 23:13:00 -0500

    First commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react'; 
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import { Feather } from '@expo/vector-icons'; 
+
+const SearchBar = () =>{
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput style={styles.inputStyle}
+                placeholder="Search"
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        marginTop: 10,
+        backgroundColor: '#E0EEEE',
+        height: 50,
+        borderRadius: 5, 
+        marginHorizontal: 15,
+        flexDirection: 'row',
+    },
+    inputStyle: {
+        // borderColor: 'black',
+        // borderWidth: 1,
+        flex: 1, 
+        fontSize: 18,
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
+    }
+});
+
+export default SearchBar;
